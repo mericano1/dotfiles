0cea2cbe3e905583e121ccbe2f634d36a473459b
Andrew Salvadore <andrew@coinrule.com>
2024-02-29 10:13:57 +0100

    adding some aliases and some additional config for iterm2

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,6 @@
 alias reload!='. ~/.zshrc'
 
+alias tf='terraform'
 alias cls='clear' # Good 'ol Clear Screen command
+alias kctl='aws-vault exec andrea -- kubectl'
+alias tf_reapply='aws-vault exec andrea --no-session -- terraform apply -var="docker_image=$(kctl -n $NAMESPACE get $DEPLOYMENT -o '"'"'jsonpath={.spec.template.spec.containers[*].image}'"'"')"' 

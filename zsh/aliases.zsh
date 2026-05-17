f48261aa59b35a92a7646c1aa3322c6dc80423d7
Andrew Salvadore <asalvadore@proda.ai>
2026-05-17 19:33:03 +0200

    Adding aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,4 +3,5 @@ alias reload!='. ~/.zshrc'
 alias tf='terraform'
 alias cls='clear' # Good 'ol Clear Screen command
 alias kctl='aws-vault exec andrea -- kubectl'
+alias cl-per='CLAUDE_CONFIG_DIR=~/.claude-personal claude'
 alias tf_reapply='aws-vault exec andrea --no-session -- terraform apply -var="docker_image=$(kctl -n $NAMESPACE get $DEPLOYMENT -o '"'"'jsonpath={.spec.template.spec.containers[*].image}'"'"')"' 
